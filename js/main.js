cb2f5cb5ddabc732825b45655e3c61e9f611159e
leonardoserra <serra.leonardo92@gmail.com>
2023-03-01 14:44:34 +0100

    prima prova.. non va

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/*  Consegna:
+    Data una lista della spesa, stampare sulla pagina (anche brutalmente,
+         basta che si vedano) gli elementi della lista 
+         individualmente con un ciclo while.
+    Consigli:
+    - Ricordiamoci di inizializzare la variabile di
+        contatore prima dell’inizio del ciclo while
+    - Ricordiamoci di incrementare il contatore, o 
+        comunque di inserire una condizione di uscita, all’interno ciclo while
+*/
+
+const list = ['pinoli','castagne','pecorino','mele','uova','plumcake',];
+const listDom = document.getElementById('list');
+
+let i;
+while(i < list.length){
+    const listElement = document.createElement('li');
+    listElement.append(list[i]);
+    listDom.innerHTML += listElement;
+
+    i++;
+
+}
+
